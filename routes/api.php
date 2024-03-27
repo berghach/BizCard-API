05e9217cad25a07a86f06b7de17742a79454af91
berghach <berghach.chemseddine@gmail.com>
2024-03-27 16:30:07 +0000

    auth is done

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,37 @@
 <?php
 
-use App\Http\Controllers\CardController;
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\LinkController;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CardController;
+use App\Http\Controllers\LinkController;
+use App\Http\Controllers\ContactController;
+use Illuminate\Validation\ValidationException;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('cards', CardController::class);
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        // 'device_name' => 'required',
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+ 
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+ 
+    return $user->createToken('admin-token', ['create', 'update', 'delete'])->plainTextToken;
+});
+
+
+Route::apiResource('cards', CardController::class)->middleware('auth:sanctum');
 Route::apiResource('contacts', ContactController::class);
 Route::apiResource('links', LinkController::class);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,41 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/setup', function(){
+    $credentials = [
+        'email' => 'admin@mail.com',
+        'password' => 'password'
+    ];
+
+    if(!Auth::attempt($credentials)){
+        $user = new User();
+
+        $user->name = 'Admin';
+        $user->email = 'admin@mail.com';
+        $user->password = Hash::make($credentials['password']);
+
+        $user->save();
+    }
+
+    if(Auth::attempt($credentials)){
+        // $user = Auth::user();
+
+        // $adminToken = $user->createToken('admin-token', ['create', 'update', 'delete']);
+        // $updateToken = $user->createToken('update-token', ['create', 'update']);
+        // $basicToken = $user->createToken('basic-token');
+        
+
+        return [
+
+        ];
+    }
 });
